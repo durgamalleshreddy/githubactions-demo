49fe07dfa230172448643326d6615eb66d15ffef
durgamalleshreddy <61488834+durgamalleshreddy@users.noreply.github.com>
2024-08-09 15:27:41 +0530

    Update ascii-script.sh

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,5 @@
 echo "my name is mallesh"
-sudo apt get install cowsay -y
+sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a dragon...RAWR" > dragon.txt
 grep -i "dragon" dragon.txt
 ls -ltra
